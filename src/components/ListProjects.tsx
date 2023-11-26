744bd9ea6ebff37a2c410e17d06febfc01c78707
konti <kontos.balazs.94@gmail.com>
2023-11-26 13:02:24 +0100

    create search for projects

--- src/components/ListProjects.tsx
+++ src/components/ListProjects.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 function ListProjects(props: any): JSX.Element {
 	const { projects, setProjects } = props;
+	const [filteredList, setFilteredList] = useState([]);
+	const searchInputRef = useRef<HTMLInputElement>(null);
 
 	useEffect(() => {
 		(async () => {
@@ -16,23 +18,84 @@ function ListProjects(props: any): JSX.Element {
 		})();
 	}, [projects]);
 
+	const handleSearchClick = () => {
+		const searchInput = searchInputRef.current?.value;
+
+		setFilteredList(
+			projects.filter(
+				(project: any) =>
+					project.title.toLowerCase().includes(searchInput) ||
+					project.description.toLowerCase().includes(searchInput)
+			)
+		);
+	};
+
 	return (
-		<div className='container-lg py-5 d-flex flex-wrap gap-4'>
-			{projects.map((project: any) => (
-				<div className='card' key={project.id}>
-					<img src={project.image} className='card-img-top' alt='' />
-					<div className='card-body d-flex flex-column pb-4'>
-						<h2 className='card-title mb-3'>{project.title}</h2>
-						<p className='card-text mb-5'>{project.description}</p>
-						<Link
-							to={'/project/' + project.id}
-							className='btn btn-primary'>
-							Részletek
-						</Link>
-					</div>
-				</div>
-			))}
-		</div>
+		<>
+			<div className='container-lg pt-5 d-flex justify-content-center'>
+				<form className='d-flex align-items-center w-50' role='search'>
+					<input
+						className='form-control me-2'
+						type='search'
+						placeholder='Keresés'
+						ref={searchInputRef}
+					/>
+					<button
+						className='btn btn-outline-primary'
+						type='button'
+						onClick={handleSearchClick}>
+						Keresés
+					</button>
+				</form>
+			</div>
+			<div className='container-lg py-5 d-flex flex-wrap gap-4'>
+				{filteredList.length > 0
+					? filteredList.map((project: any) => (
+							<div className='card' key={project.id}>
+								<img
+									src={project.image}
+									className='card-img-top'
+									alt=''
+								/>
+								<div className='card-body d-flex flex-column pb-4'>
+									<h2 className='card-title mb-3'>
+										{project.title}
+									</h2>
+									<p className='card-text mb-5'>
+										{project.description}
+									</p>
+									<Link
+										to={'/project/' + project.id}
+										className='btn btn-primary'>
+										Részletek
+									</Link>
+								</div>
+							</div>
+					  ))
+					: projects.map((project: any) => (
+							<div className='card' key={project.id}>
+								<img
+									src={project.image}
+									className='card-img-top'
+									alt=''
+								/>
+								<div className='card-body d-flex flex-column pb-4'>
+									<h2 className='card-title mb-3'>
+										{project.title}
+									</h2>
+									<p className='card-text mb-5'>
+										{project.description}
+									</p>
+									<Link
+										to={'/project/' + project.id}
+										className='btn btn-primary'>
+										Részletek
+									</Link>
+								</div>
+							</div>
+					  ))}
+			</div>
+		</>
 	);
 }
 
